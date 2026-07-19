a99159ea745c49c3a33a9491180abeef63cd7584
facusalass <facusalass15@gmail.com>
2026-07-18 22:04:27 -0300

    mejoras de rendimiento

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 import icon from 'astro-icon';
 
+const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
+
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'https://example.com',
+  site: env.PUBLIC_SITE_URL || 'https://example.com',
+
+  build: {
+    inlineStylesheets: 'always',
+  },
 
   vite: {
     plugins: [tailwindcss()],
